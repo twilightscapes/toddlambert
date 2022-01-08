a4eb25db0561f1a115015f1ce24dc1a4a11d2bde
twilightscapes <ihatespam@gmail.com>
2022-01-08 03:22:56 -0600

    tweaks

--- src/pages/test2.js
+++ src/pages/test2.js
@@ -107,7 +107,7 @@ export default function test2Page() {
 
 
 <SRLWrapper options={options} className="">
-  <div className="sliderholder" style={{display:'flex', justifyContent:'center', width:'100%', height:'355px', overflow:'hidden', position:'relative', padding:'20px 0'}}>
+  <div className="sliderholder" style={{display:'flex', justifyContent:'center', width:'100%', height:'355px', overflow:'hidden', position:'relative', padding:'0 0'}}>
 <div class="horizontal-scroll-wrapper squares" style={{margin:'0 auto', width:'', padding:'0'}}>
   {/* <div>item 1</div>
   <div>item 2</div>
